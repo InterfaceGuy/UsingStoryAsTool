9f7262bfaa5faef9e139354c05ee3df4007f86e7
Interface Guy <david.rug98@icloud.com>
2024-08-20 14:16:17 +0100

    initial commit

---
+++ .pl
@@ -0,0 +1,6 @@
+{
+  "dateCreated": "2024-08-15T18:31:41.695Z",
+  "dateModified": "2024-08-15T18:31:41.695Z",
+  "interactions": 0,
+  "type": "idea"
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# DreamNode
+# UsingStoryAsTool
+![](UsingStoryAsTool.png)
